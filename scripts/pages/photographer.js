64359f4af02fe65cac9507889bf91bef686026b9
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-06-28 17:10:28 +0200

    feat: ajout des données d'1 photographe sur la page photographer.html refactor: ajout de commentaires pour ordonner le CSS
    La page photographer fait aussi appel au script photographerTemplate.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,25 @@
-//Mettre le code JavaScript lié à la page photographer.html
+    async function getPhotographers() {
+        // Récupération des données JSON
+        const response = await fetch('./data/photographers.json');
+        const photographers = await response.json();
+        // Retourne le tableau des données une fois récupéré
+        return photographers;
+    }
+
+    async function displayData(photographer) {
+        const photographersSection = document.querySelector(".photograph-header");
+
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    }
+
+    async function init() {
+        // Récupère les datas du photographe sélectionné
+        const { photographers } = await getPhotographers();
+        const photographer = photographers.find(photographer => photographer.id === 243)
+        // Creation et affichage du profil du photographe
+        displayData(photographer);
+    }
+
+    init();
