fb6d5e2fb5ee33a878cc367d3d757fe7c9eec383
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-07-05 15:07:10 +0200

    feat: ajout d'un effet au survol de la souris et au focus des liens en index et des boutons. feat: ajout d'une animation douce à l'apparition et disparition de la modale.
    Les boutons principaux sont dans un fichier css à part.
    Le style de la modal est dans un fichier css à part.

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,35 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+/********************* ELEMENTS DU DOM ********************/
+const modal = document.getElementById("contact_modal");
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+const animationDelay = 500;
+
+
+/*********************  */
+async function opacityDeseappear(DOMEelement) {
+    DOMEelement.style.animationName = "opacity-desappear";
+    DOMEelement.style.animationDuration = `${animationDelay}ms`;
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+async function opacityAppear(DOMEelement) {
+    DOMEelement.style.animationName = "opacity-appear";
+    DOMEelement.style.animationDuration = `${animationDelay}ms`;
+}
+
+async function displayModal() {
+    await opacityAppear(modal);
+    modal.style.display = "block";
+    modal.setAttribute("aria-hidden", "false");
+    main.setAttribute("aria-hidden", "true");
+    header.setAttribute("aria-hidden", "true");
+}
+
+async function closeModal() {
+    await opacityDeseappear(modal);
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, animationDelay);
+    modal.setAttribute("aria-hidden", "true");
+    main.setAttribute("aria-hidden", "false");
+    header.setAttribute("aria-hidden", "false");
 }
